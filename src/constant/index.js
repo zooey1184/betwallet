615e168879422cc1453af61d7f887f858ffed1b8
yingyingzhang <zooey.zhang@igg.com>
2022-05-16 16:29:54 +0800

    feat: update

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,7 +1,7 @@
 import {abi} from './Bonus'
 
 // export const ADDRESS = '0x1b8fd9f757d3a358b0C41ce03A0E4676D87BC522'
-export const ADDRESS = '0x6FB75E1455aBe9a0E7cc11ACEc5dd8D668e9486D'
+export const ADDRESS = '0x3b91c659141EA81290DaCA2eb2D761795b65b077'
 export const DEPOSIT = 'deposit'
 export const EXTRACT = 'withdrawInterest'
 export const ABI = abi
