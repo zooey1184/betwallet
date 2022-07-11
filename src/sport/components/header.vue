348d1edb0f5fa571f32a3414e081e22c4883ae51
yingyingzhang <zooey.zhang@igg.com>
2022-07-11 23:18:09 +0800

    feat: update

--- src/sport/components/header.vue
+++ src/sport/components/header.vue
@@ -1,105 +1,127 @@
 <template>
   <header class="zyqhd flexC Huans">
-		<a href="javascript:;" class="wapNav" @click='handleShowNav'></a>
-		<div class="zyqlogoK Huans flexC">			
-			<a href="javascript:;" class="zyqlogo flexC"><img src="../images/zyq0425/logo.png"></a>
-		</div>
-		<div class="hdNavK flexC">
-			<a href="javascript:;"
-        @click='handlePick(item)'
-        v-for='item in options' 
-        :class="{'on': state.active === item.value}" 
-        class="hdNavA">
-        <i class="hdNavI" v-if='item.icon' :class="item.icon"></i>
-        <p>{{item.label}}</p>
+    <a href="javascript:;" class="wapNav" @click="handleShowNav"></a>
+    <div class="zyqlogoK Huans flexC">
+      <a href="javascript:;" class="zyqlogo flexC"
+        ><img src="../images/zyq0425/logo.png"
+      /></a>
+    </div>
+    <div class="hdNavK flexC">
+      <a
+        href="javascript:;"
+        @click="handlePick(item)"
+        v-for="item in options"
+        :class="{ on: state.active === item.value }"
+        class="hdNavA"
+      >
+        <i class="hdNavI" v-if="item.icon" :class="item.icon"></i>
+        <p>{{ item.label }}</p>
       </a>
-		</div>
+    </div>
 
-		<div class="zyqhdR flexC">
+    <div class="zyqhdR flexC">
+      <div class="zyqhdwal flexC fl-bet">
+        <div class="text-align-center w-100p">激活房间号</div>
+      </div>
       <!-- wallet -->
-			<div class="zyqhdwal flexC fl-bet" v-if='isLink'>
-				<i class="zyqhdwalI"></i>
-				<div class="qhdwalN flexC">
-					<p>0</p>
-					<span>$0</span>
-				</div>
-				<a href="javascript:;" class="zyqhdwala IndBac1">wallet</a>
-			</div>
+      <div class="zyqhdwal flexC fl-bet" v-if="isLink">
+        <i class="zyqhdwalI"></i>
+        <div class="qhdwalN flexC">
+          <p>0</p>
+          <span>$0</span>
+        </div>
+        <a href="javascript:;" class="zyqhdwala IndBac1">wallet</a>
+      </div>
       <!-- info -->
-			<div class="zyqhdXD" v-if='isLink'>
-				<a href="javascript:;" class="zyqhdId flexC fl-bet Huans">
-					<div class="RightHdI"><img src="../images/zyq0425/pic01.jpg"></div>
-					<div class="RightHdN">
-						<p>{{getId}}</p>
-						<div class="RightHdJ"></div>
-					</div>
-				</a>
-			</div>	
-      
+      <div class="zyqhdXD" v-if="isLink">
+        <a href="javascript:;" class="zyqhdId flexC fl-bet Huans">
+          <div class="RightHdI"><img src="../images/zyq0425/pic01.jpg" /></div>
+          <div class="RightHdN">
+            <p>{{ getId }}</p>
+            <div class="RightHdJ"></div>
+          </div>
+        </a>
+      </div>
+
       <!-- not login  -->
-			<a href="javascript:;" v-if='!ACCOUNTS.isLink' class="hdBut IndBac1" @click='handleConnect'>link wallet</a>
-		</div>
-	</header>
+      <a
+        href="javascript:;"
+        v-if="!ACCOUNTS.isLink"
+        class="hdBut IndBac1"
+        @click="handleConnect"
+        >link wallet</a
+      >
+    </div>
+  </header>
 </template>
 
 <script>
-import { computed, defineComponent, inject, reactive, ref, watch } from 'vue'
+import { computed, defineComponent, inject, reactive, ref, watch } from "vue";
 
 export default defineComponent({
   components: {},
   props: {
     userInfo: {
-      type: Object
+      type: Object,
     },
     ID: {
-      type: [String, Number]
+      type: [String, Number],
     },
     active: {
-      type: String
+      type: String,
     },
     options: {
-      type: Array
-    }
+      type: Array,
+    },
   },
-  emits: ['link', 'update:active'],
-  setup(props, {emit}) {
+  emits: ["link", "update:active"],
+  setup(props, { emit }) {
     const state = reactive({
       active: props.active,
-    })
+    });
 
-    const SPORTS = inject('SPORTS')
+    const SPORTS = inject("SPORTS");
 
-    const ACCOUNTS = inject('ACCOUNTS')
+    const ACCOUNTS = inject("ACCOUNTS");
     const isLink = computed(() => {
-      return ACCOUNTS.isLink.value
-    })
+      return ACCOUNTS.isLink.value;
+    });
 
-    watch(() => props.active, (n) => {
-      state.active = n
-    }, {
-      immediate: true
-    })
-    watch(() => state.active, (n) => {
-      emit('update:active', n)
-    })
+    watch(
+      () => props.active,
+      (n) => {
+        state.active = n;
+      },
+      {
+        immediate: true,
+      }
+    );
+    watch(
+      () => state.active,
+      (n) => {
+        emit("update:active", n);
+      }
+    );
 
-    const getId = computed(() => ACCOUNTS.accountHide.value)
+    const getId = computed(() => ACCOUNTS.accountHide.value);
 
     const handlePick = (item) => {
-      state.active = item.value
-    }
+      state.active = item.value;
+    };
 
     const handleConnect = () => {
-      emit('link')
-    }
+      emit("link");
+    };
 
     const handleShowNav = () => {
-      if(!$('.wapNav').hasClass('wapNavO')){
-        $('.wapNav').addClass('wapNavO');$(".wapMenu").slideDown(100);
-      }else{
-        $('.wapNav').removeClass('wapNavO');$('.wapMenu').slideUp(100);
+      if (!$(".wapNav").hasClass("wapNavO")) {
+        $(".wapNav").addClass("wapNavO");
+        $(".wapMenu").slideDown(100);
+      } else {
+        $(".wapNav").removeClass("wapNavO");
+        $(".wapMenu").slideUp(100);
       }
-    }
+    };
 
     return {
       state,
@@ -109,8 +131,8 @@ export default defineComponent({
       handleShowNav,
       getId,
       SPORTS,
-      isLink
-    }
-  }
-})
+      isLink,
+    };
+  },
+});
 </script>
