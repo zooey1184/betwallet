4f4a33fc53729fa8d77524e76b912efc81ab2f2c
yingyingzhang <zooey.zhang@igg.com>
2022-07-28 00:40:38 +0800

    feat: update

--- src/sport/components/room/confirm-id.vue
+++ src/sport/components/room/confirm-id.vue
@@ -1,57 +1,59 @@
 <template>
   <div>
-    <div class="color-gray font-size-18 font-weight-600">
-      需要确认身份
-      <question-circle-outlined class="font-size-16" />
+    <div class="active-color font-size-18 font-weight-600 text-align-center">
+      IDENTITY
+      <div>CONFIRMATION REQUIRED</div>
     </div>
-    <div class="mt-48 mb-16">
-      <Circle>
-        <img class="pos-a" style="top: 0; width: 160px; top: 0" src="../../images/zyq0425/zyqTcN01.png" />
-      </Circle>
+    <div class="mt-8 mb-16 flex justify-center">
+      <img style="width: 200px" src="../../images/v2/room-wallet.png" />
     </div>
-    <div class="text-align-center color-gray">您必须前往钱包确认身份验证才能确认创建</div>
+    <div class="text-align-center line-height-18">
+      CREATING REQUIRES
+      <div>YOU TO VERIFY AUTHENTICATION IN YOUR WALLET</div>
+    </div>
 
-    <div class="flex items-center justify-center mt-48 mb-48">
-      <img class="spin" src="../../images/zyq0425/zyqTcNs.png">
-      <div class="font-size-18 font-weight-600 ml-16">签名请求</div>
+    <div class="flex items-center justify-center m-24">
+      <img
+        class="spin"
+        style="width: 50px"
+        src="../../images/v2/room-loading.png"
+      />
+      <!-- <div class="font-size-18 font-weight-600 ml-16">签名请求</div> -->
     </div>
   </div>
 </template>
 
 <script>
-  import { reactive, defineComponent } from 'vue'
-  import { QuestionCircleOutlined, LoadingOutlined} from '@ant-design/icons-vue'
-  import Circle from './circle.vue'
+import { reactive, defineComponent } from "vue";
+import { QuestionCircleOutlined, LoadingOutlined } from "@ant-design/icons-vue";
+import Circle from "./circle.vue";
 
-  export default defineComponent({
-    components: {
-      QuestionCircleOutlined,
-      LoadingOutlined,
-      Circle
-    },
-    setup() {
-      const state = reactive({})
+export default defineComponent({
+  components: {
+    QuestionCircleOutlined,
+    LoadingOutlined,
+    Circle,
+  },
+  setup() {
+    const state = reactive({});
 
-      
-
-      return {
-        state
-      }
-    }
-  })
+    return {
+      state,
+    };
+  },
+});
 </script>
-
 
 <style lang="less" scoped>
 .spin {
-	animation: loading 3s linear infinite;
+  animation: loading 3s linear infinite;
 }
 @keyframes loading {
-	from {
-		transform: rotate(0deg);
-	}
-	to {
-		transform: rotate(360deg);
-	}
+  from {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(0);
+  }
 }
 </style>
